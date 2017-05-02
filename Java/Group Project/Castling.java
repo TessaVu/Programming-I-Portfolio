280b4d5f375c12bba6e5f753a0caa9eea632ee3d
Tessa Vu <vutessa.tv@gmail.com>
2017-05-02 12:24:50 -0600

    Update Castling.java

--- Java/Group Project/Castling.java
+++ Java/Group Project/Castling.java
@@ -1,61 +1,48 @@
 public class King extends Piece {
-  public String color;
+	public String color;
 	public boolean moved;
 	public boolean castle;
-  public King(boolean available, int x, int y) {
-    super(availabe, x, y);
-    this.moved = false;
-    this.castle = false;
-  }
+	public King(boolean available, int x, int y) {
+		super(availabe, x, y);
+		this.moved = false;
+		this.castle = false;
+	}
   
-  @Override
-  public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
-    if(super.isValid(board, fromX, fromY, toX, toY) == false) {
-      return false;
-    }
-    if(Math.sqrt(Math.pow(Math.abs((toX - fromX)),2)) + Math.pow(Math.abs((toY - fromY)), 2) != Math.sqrt(2)){
-      return false;
-    }
-    return false;
-  }
-  
+@Override
+public boolean isValid(Board board, int fromX, int fromY, int toX, int toY) {
+	if(super.isValid(board, fromX, fromY, toX, toY) == false) {
+		return false;
+	}
+	if(Math.sqrt(Math.pow(Math.abs((toX - fromX)),2)) + Math.pow(Math.abs((toY - fromY)), 2) != Math.sqrt(2)) {
+		return false;
+	}
+		if(moved) {
+			return false;
+		}
+	return false;
 }
-	
-
-
-	@Override
-	public boolean validateMove(Piece[][] board, int currentRow, int currentCol, int newRow, int newCol) {
-		
-		if(Math.abs(newRow - currentRow) > 1 || Math.abs(newCol - currentCol) > 1){
-			
-			if(hasMoved){
+//
+if(fromY - toY == 2 && fromX == toX) {
+	if(board[fromX][fromY + 1] != null || board[toX][toY + 2] != null) {
+		castle = false;
+		return false;
+	}
+		} else if(fromY - toY == 3 && fromX == toX){
+			if(board[toX][fromY - 1] != null || board[toX][fromY - 2] != null || board[toX][fromY - 3] != null){
+				castle = false;
+				return false;
+				}
+				
+			} else {
+				castle = false;
 				return false;
 			}
 			
-			//Do castling logic here
-			if(newCol - currentCol == 2 && currentRow == newRow){
-				//Castle kingside
-				if(board[newRow][currentCol + 1] != null || board[newRow][currentCol + 2] != null){
-					castled = false;
-					return false;
-				}
-				
-			}else if(currentCol - newCol == 3 && currentRow == newRow){
-				if(board[newRow][currentCol - 1] != null || board[newRow][currentCol - 2] != null || board[newRow][currentCol - 3] != null){
-					castled = false;
-					return false;
-				}
-				
-			}else{
-				castled = false;
-				return false;
-			}
-			
-			castled = true;
+			castle = true;
 			
 		}
 		
-		//hasMoved = true;
+		//moved = true;
 		return true;
 	}
 	
